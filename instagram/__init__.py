5843d39d34303b047fcafe06a58c1cfd711f6c8d
erudzitis <rudzitisernests1@gmail.com>
2019-05-14 18:12:43 +0300

    'design'

--- instagram/__init__.py
+++ instagram/__init__.py
@@ -1,8 +1,13 @@
 import os
+import logging
 
 from instagram.application import create_application
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 configuration = os.environ['APPLICATION_CONFIG_FILE']
 
 application = create_application(configuration=configuration)
+
+logger.info("Hello world")
